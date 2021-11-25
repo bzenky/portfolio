75755f62ef3e95f11d3c25ea1de77afa6f3f0c54
Zenky <bzenky@gmail.com>
2021-11-25 19:51:05 -0300

    hero desktop background, github last updates mobile

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useMediaQuery } from 'react-responsive';
 import Image from 'next/image'
 import axios from 'axios'
 import Fade from 'react-reveal/Fade'
@@ -11,21 +12,26 @@ import linkIcon from '../../../public/linkIcon.svg'
 const Hero = () => {
   const [repositories, setRepositories] = useState([])
 
+  const isShortIndex = useMediaQuery({ query: `(max-width: 1150px)` })
+  const dynamicIndex = isShortIndex ? 3 : 6
+
+  
   useEffect(() => {
     axios.get('https://api.github.com/users/bzenky/repos?sort=updated&direction=des')
-      .then(response => {
-        const data = response.data
-
-        setRepositories(data)
-      })
+    .then(response => {
+      const data = response.data
+      
+      setRepositories(data)
+    })
   }, [])
-
+  
   return (
     <div className={styles.projects}>
       <Fade>
         <h2 className={styles.projectsTitle}>Últimas Atualizações Github</h2>
         <div div className={styles.projectsCards}>
-          {repositories.map((repository, index) => index < 6 && (
+          {repositories.map((repository, index) => index < dynamicIndex && (
+
             <div className={styles.projectCard} key={repository.id}>
               <h3 className={styles.projectTitle}>
                 {repository.name}
